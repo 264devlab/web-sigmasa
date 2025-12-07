ae775d07502124f30f3689ec951b85a0c1edc9d2
ITerluk <ismaterluk98@gmail.com>
2025-12-07 00:08:56 -0300

    Routing Lazy Loading and Main Layout

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './shared/layouts/main-layout/main-layout';
+import { mainRoutes } from './core/routes/main.routes';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        component: MainLayoutComponent,
+        children: mainRoutes,
+    },
+    {
+        path: '**',
+        redirectTo: 'home',
+        pathMatch: 'full',
+    }
+];
---
+++ src/app/shared/layouts/error-layout/error-layout.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-error-layout',
+  imports: [],
+  templateUrl: './error-layout.html',
+  styleUrl: './error-layout.scss',
+})
+export class ErrorLayout {
+
+}
