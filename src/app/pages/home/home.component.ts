33fb93ea17a5854f3f73fce2b72c7efa336517b5
gonzalobalderrama-it <gonzalobalderrama.it@gmail.com>
2026-03-05 19:08:09 -0300

    FEATURE 41 | Sección clientes
    * feat: Add clients component to the home page and include new client logos.
    
    * feat: Add styles for the clients component, including a logo carousel with responsive adjustments.

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { I18nPipe } from '../../shared/i18n/i18n.pipe';
 import { ProjectsMap } from '../../shared/components/projects-map/projects-map';
+import { ClientsComponent } from '../../shared/components/clients/clients.component';
 
 @Component({
   selector: 'app-home',
-  imports: [I18nPipe, ProjectsMap],
+  imports: [I18nPipe, ProjectsMap, ClientsComponent],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
---
+++ src/app/shared/components/clients/clients.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-clients',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './clients.component.html',
+  styleUrl: './clients.component.scss'
+})
+export class ClientsComponent {
+  logos: string[] = [
+    'assets/logos/clientes/adium.svg',
+    'assets/logos/clientes/barrick.svg',
+    'assets/logos/clientes/impregilo.svg',
+    'assets/logos/clientes/isolux-corsan.svg',
+    'assets/logos/clientes/libertad.svg',
+    'assets/logos/clientes/salentein.svg',
+    'assets/logos/clientes/yamana_gold.svg',
+  ];
+
+  // Doubling the logos to create the infinite scroll effect
+  scrollingLogos = [...this.logos, ...this.logos];
+}
